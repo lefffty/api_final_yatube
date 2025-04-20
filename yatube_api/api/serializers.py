5c05c2fe3ad331b53d9cea1837d752be6ad79238
leffty <brigante2004@yandex.ru>
2025-04-20 17:20:07 +0300

    v1.4.2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -17,14 +17,7 @@ class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = (
-            'id',
-            'author',
-            'text',
-            'pub_date',
-            'image',
-            'group',
-        )
+        fields = ('id', 'author', 'text', 'pub_date', 'image', 'group')
         model = Post
 
 
@@ -34,27 +27,14 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = (
-            'id',
-            'author',
-            'text',
-            'created',
-            'post',
-        )
-        read_only_fields = (
-            'post',
-        )
+        fields = ('id', 'author', 'text', 'created', 'post')
+        read_only_fields = ('post', )
         model = Comment
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = (
-            'id',
-            'title',
-            'slug',
-            'description',
-        )
+        fields = ('id', 'title', 'slug', 'description')
         model = Group
 
 
@@ -73,3 +53,34 @@ class FollowSerializer(serializers.ModelSerializer):
             'following',
         )
         model = Follow
+
+    def validate_following(self, value):
+        """
+        Нелья подписаться на самого себя
+        """
+        request_user = self.context['request'].user
+        if request_user == value:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя'
+            )
+        return value
+
+    def validate(self, attrs):
+        """
+        Нельзя подписаться на одного пользователя дважды
+        """
+        user = self.context['request'].user
+        if Follow.objects.filter(user=user, following=attrs['following']).exists():
+            raise serializers.ValidationError(
+                'Вы уже подписаны на этого пользователя'
+            )
+        return attrs
+
+    def create(self, validated_data):
+        """
+        Создание подписки
+        """
+        return Follow.objects.create(
+            user=self.context['request'].user,
+            following=validated_data['following'],
+        )
